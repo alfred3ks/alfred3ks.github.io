971d856777a610a2ef1c13120f43d09e49a5c5c7
Alfredo Sánchez <alfred3ks@gmail.com>
2021-10-28 15:41:55 +0200

    Agrego funcionalidad con JS al menu hamburguesa en mobile.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Obtenemos boton y lista
+const button = document.getElementById('button');
+const list= document.getElementById('list');
+const ul = document.getElementById('list__hide');
+
+// Creamos la funcion para el menu hamburguesa
+const change = ()=>{
+    button.classList.toggle('change')
+    list.classList.toggle('change__menu')
+}
+
+button.addEventListener('click', change);
+ul.addEventListener('click', change);
