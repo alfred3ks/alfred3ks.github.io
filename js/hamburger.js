5a7e6a3f26439a75cdc354fd138dc33269424a85
Alfredo Sánchez <alfred3ks@gmail.com>
2021-10-29 13:32:28 +0200

    Hago refactoring de las clases mas sencillas.

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,8 @@
 // Obtenemos boton y lista
 const button = document.getElementById('button');
-console.log(button);
 const list= document.getElementById('list');
 const ul = document.getElementById('list__hide');
 
-const about = document.getElementById('about');
 
 // Creamos la funcion para el menu hamburguesa
 const change = ()=>{
@@ -14,13 +12,4 @@ const change = ()=>{
 
 button.addEventListener('click', change);
 ul.addEventListener('click', change);
-
-// Ir a sobre mi
-const sobreMi = ()=>{
-        window.scrollTo({
-        top:0,
-        behavior:'smooth'
-    })
-}
-about.addEventListener('click', sobreMi);
 
---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+const about = document.getElementById('about');
+
+// Ir a sobre mi
+const sobreMi = ()=>{
+        window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}
+about.addEventListener('click', sobreMi);
