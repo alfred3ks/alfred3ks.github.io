a6609ea8c10cb22cabddeb0b2c7f0498268e784a
Alfredo Sánchez <alfred3ks@gmail.com>
2023-02-19 11:57:07 +0100

    Hago cambios en fecha.js quito innerHTML por textContent.

--- js/fecha.js
+++ js/fecha.js
@@ -3,8 +3,8 @@ const dato = document.querySelector('#fecha');
 let fecha = new Date();
 let actual = fecha.getFullYear();
 
-let cambiarAnnio = ()=>{
-    dato.innerHTML = actual;
+let cambiarAnnio = () => {
+    dato.textContent = actual;
 }
 
 export default cambiarAnnio;
