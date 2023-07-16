1867a9c65648501783015553ad49e3ba4f6e17ea
Alfredo Sánchez <alfred3ks@gmail.com>
2023-07-16 17:10:09 +0200

    Creo los estilos en header.css para movil, tablet y desktop.

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,23 +1,21 @@
 // Obtenemos boton y lista
-const button = document.querySelector('#button');
-const list= document.querySelector('#list');
-const ul = document.querySelector('#list__hide');
-
+const button = document.querySelector("#button");
+const list = document.querySelector("#list");
+const ul = document.querySelector("#list__hide");
 
 // Creamos la funcion para el menu hamburguesa
-const change = ()=>{
-    button.classList.toggle('change')
-    list.classList.toggle('change__menu')
-}
+const change = () => {
+  button.classList.toggle("change");
+  list.classList.toggle("change__menu");
+};
 
 // button.addEventListener('click', change);
 // ul.addEventListener('click', change);
 
-let ejecutarCambio = ()=>{
-    button.addEventListener('click', change);
-    ul.addEventListener('click', change);
-}
+let ejecutarCambio = () => {
+  button.addEventListener("click", change);
+  ul.addEventListener("click", change);
+};
 
 // ejecutarCambio();
 export default ejecutarCambio;
-
